354af046ac98305635b583a4387741d6e5fab4e3
Nagendra-Chowdary <chowdarynagendra006@gmail.com>
2024-12-22 11:05:21 +0530

    Reversing a single linked list

--- 45_linkedList_problems/reverseLinkedList.cpp
+++ 45_linkedList_problems/reverseLinkedList.cpp
@@ -12,12 +12,9 @@ class Node{
 void reverseLL(Node* &node){
     Node* temp=node;
     Node* prev=NULL;
-    Node* p;
+    Node* p=NULL;
     while(temp!=NULL){
      p=temp->next;
-    cout<<prev->data<<endl;
-    cout<<temp->data<<endl;
-    cout<<p->data<<endl;
      temp->next=prev;
      prev=temp;
      temp=p;
@@ -25,6 +22,13 @@ void reverseLL(Node* &node){
 
     }
     node=prev;
+}
+void print(Node* &node){
+  Node* head=node;
+  while(head!=NULL){
+    cout<<head->data<<endl;
+    head=head->next;
+  }
 }
 int main(){
 Node* node=new Node(9);
@@ -34,10 +38,9 @@ Node* node3=new Node(48);
 node2->next=node3;
 Node* node4=new Node(10);
 node3->next=node4;
+Node* node5=new Node(910);
+node4->next=node5;
 reverseLL(node);
-// cout<<node->data<<endl;
-// cout<<node2->data<<endl;
-// cout<<node3->data<<endl;
-// cout<<node4->data<<endl;
+print(node);
 
 }
