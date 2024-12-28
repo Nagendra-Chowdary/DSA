32e9d3e63b728ff86f0590154349f7cd8226defd
Nagendra-Chowdary <chowdarynagendra006@gmail.com>
2024-12-28 12:03:16 +0530

    Reverse a stack using recursion

---
+++ 55_Stack_Interview_Questions/reverseRecur.cpp
@@ -0,0 +1,58 @@
+//The very same logic as deleting a mid element.
+#include<iostream>
+#include<stack>
+
+using namespace std;
+//Reverse a stack using recursion.
+void insertBott(stack<int> &s,int ele){
+    if(s.empty()){
+        //    cout<<S.top()<<endl;
+         s.push(ele);
+           return;
+    }
+    else{
+        int t=s.top();
+      s.pop();
+        insertBott(s,ele);
+        s.push(t);
+    }
+}
+void rev(stack<int> &s){
+    if(s.empty()){
+    
+           return;
+    }
+    else{
+        int t=s.top();
+      s.pop();
+        rev(s);
+        insertBott(s,t);
+    }
+}
+
+
+
+//stack is a data structure that follows LIFO.
+int main(){
+    stack<int> s;
+    s.push(9);
+    s.push(90);
+    s.push(90);
+    s.push(900);
+    s.push(90);
+    s.push(90);
+    s.push(900);
+    s.push(990);
+    s.pop();
+    // int t=s.top();
+    bool a=s.empty();
+    // cout<<a<<endl;
+    // cout<<t<<endl;
+    insertBott(s,7);
+    rev(s);
+    cout<<s.top()<<endl;
+    
+    
+
+
+}
